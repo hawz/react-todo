357cefb995b74948bf07a60a477c54414dcdc284
Alberto Masia <alberto.masia@gmail.com>
2019-08-06 11:01:10 +0200

    CSS tweaks to list and container

--- src/App.js
+++ src/App.js
@@ -5,7 +5,21 @@ import TodoItem from './components/TodoItem/TodoItem';
 
 function App() {
   const style = {
-    padding: 20
+    backgroundColor: "white",
+    margin: "auto",
+    width: "50%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    padding: 30,
+    border: "1px solid #efefef",
+    boxShadow: `
+    0 1px 1px rgba(0,0,0,0.15),
+    0 10px 0 -5px #eee,
+    0 10px 1px -4px rgba(0,0,0,0.15),
+    0 20px 0 -10px #eee,
+    0 20px 1px -9px rgba(0,0,0,0.15)
+    `
   };
 
   return (
